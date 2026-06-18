a28aa678c814e09b6aed3d0d62bbcd7a04ae5b6c
Ericson <60470500+ericson-j-santos@users.noreply.github.com>
2026-06-17 22:00:27 -0300

    feat(outbox): adiciona status pendente redmine

--- apps/reqsys-enterprise-api/src/main/java/br/com/reqsys/enterprise/domain/StatusRequisito.java
+++ apps/reqsys-enterprise-api/src/main/java/br/com/reqsys/enterprise/domain/StatusRequisito.java
@@ -1,5 +1,10 @@
 package br.com.reqsys.enterprise.domain;
 
 public enum StatusRequisito {
-    VALIDADO, ESTRUTURADO, PUBLICADO_REDMINE, PUBLICADO_WIKI, ERRO
+    VALIDADO,
+    ESTRUTURADO,
+    PUBLICACAO_REDMINE_PENDENTE,
+    PUBLICADO_REDMINE,
+    PUBLICADO_WIKI,
+    ERRO
 }
